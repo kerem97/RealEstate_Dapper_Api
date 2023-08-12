2af008a9c6507980c71e65bd3c76511bc58ee5fe
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-08-12 17:23:16 +0300

    updated

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductListComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using static System.Collections.Specialized.BitVector32;
+using System;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultHomePageProductListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
