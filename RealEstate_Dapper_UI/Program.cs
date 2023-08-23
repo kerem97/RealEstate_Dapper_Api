a101e982ec77e0cb0516118c3695eebc389deddb
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-08-23 11:48:50 +0300

    updated

--- RealEstate_Dapper_UI/Program.cs
+++ RealEstate_Dapper_UI/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHttpClient();
 builder.Services.AddMvc().AddRazorRuntimeCompilation();
 builder.Services.AddControllersWithViews();
 
--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductListComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductListComponentPartial.cs
@@ -2,13 +2,30 @@
 using Microsoft.AspNetCore.Mvc;
 using static System.Collections.Specialized.BitVector32;
 using System;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultHomePageProductListComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultHomePageProductListComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44324/api/Products/ProductListWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDtos>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
