e3375362c4250cee9c082d6cf9efab57a2a98e27
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-09-04 17:32:01 +0300

    whoWeAre Api Consumed

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultServicesComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultServicesComponentPartial.cs
@@ -4,6 +4,13 @@ namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultServicesComponentPartial : ViewComponent
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultServicesComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IViewComponentResult Invoke()
         {
             return View();
